32f844879bcd8ed2691cf9543eb27554bb73f625
Thanh Ha Ng Le <harley.ngx@gmail.com>
2020-04-13 18:40:13 +0700

    Add instruction to push

--- src/components/store/Item.jsx
+++ src/components/store/Item.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
 export default class MenuItem extends Component {
     render() {
+        const url = `/product/${this.props.id}`;
         return (
             <div className="item-container">
                 <div className="item-img">
-                    <a href="http://localhost:3000/menu/">
+                    <Link to={url}>
                         <img src={this.props.img_link} alt={this.props.desc}/>
-                    </a>
+                    </Link>
                 </div>
                 <div className="item-content">
                     <div className="item-name">{this.props.name}</div>
