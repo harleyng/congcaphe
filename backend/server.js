1bd1bb367166f5331648f9dc0e814610d5023aca
Thanh Ha Ng Le <harley.ngx@gmail.com>
2020-04-30 02:48:27 +0700

    Link DB with project and write API to render Shop Page Closes #12

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const menuApiRouter = require('./routers/shop/menu')
+const groceryApiRouter = require('./routers/shop/grocery')
 
 mongoose.connect(
-    'mongodb+srv://congadmin:11223344@congcaphe-e0q94.mongodb.net/test?retryWrites=true&w=majority',
+    'mongodb://localhost/congcaphe',
     { useUnifiedTopology: true, useNewUrlParser: true },
     (err) => {
     if (err) console.log(err);
@@ -13,10 +15,14 @@ mongoose.connect(
         console.log('Connect to DB success')
         const app = express();
 
+        app.use(cors({
+            origin: 'http://localhost:3000'
+        }));
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended:false }));
 
         app.use('/api/menu', menuApiRouter);
+        app.use('/api/grocery', groceryApiRouter);
 
         app.listen(3001, (err) => {
             if (err) console.log(err);
