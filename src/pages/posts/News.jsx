8266a8fdb5949d70b270a6030a90083e1cf3856d
Thanh Ha Ng Le <harley.ngx@gmail.com>
2020-04-30 02:46:31 +0700

    White box testing #18

--- src/pages/posts/News.jsx
+++ src/pages/posts/News.jsx
@@ -2,8 +2,45 @@ import React, { Component } from 'react'
 import {Helmet} from 'react-helmet';
 import Category from '../../components/posts/Category'
 import logo from '../../assets/img/logo.png'
+import $ from 'jquery'
+import {setAttributes} from './setAttributes'
+var nodemailer = require('nodemailer');
 
 export default class News extends Component {
+        // sendMail() {
+        //     let mail = $('#registeredEmail');
+        //     let protocol = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(mail.value);
+        //     if (!mail.value) {
+        //         setAttributes(mail, {"data-toogle": "tooltip", "title": "You have entered nothing"});
+        //     } else {
+        //         if (protocol == false) {
+        //             setAttributes(mail, {"data-toogle": "tooltip", "title": "You have entered invalid email"});
+        //         } else {
+        //         var transporter = nodemailer.createTransport({
+        //             service: 'gmail',
+        //             auth: {
+        //             user: 'harley.ngx@gmail.com',
+        //             pass: 'yourpassword'
+        //             }
+        //         });
+                
+        //         var mailOptions = {
+        //             from: 'harley.ngx@gmail.com',
+        //             to: mail,
+        //             subject: 'You have registered to follow Cong',
+        //             text: 'Enjoy coffee'
+        //         };
+                
+        //         transporter.sendMail(mailOptions, function(error, info){
+        //             if (error) {
+        //             console.log(error);
+        //             } else {
+        //             console.log('Email sent: ' + info.response);
+        //             }
+        //         });
+        //     }
+        // }
+    
     render() {
         return (
             <div className="page Container">
@@ -21,8 +58,8 @@ export default class News extends Component {
                                 <div><img src={logo} alt="logo Cộng"/></div>
                                 <div className="followPosts-desc"><p>Nhận những thông tin mới nhất từ Cộng Cà Phê.</p></div>
                                 <div className="emailRegister-posts-container">
-                                    <input className="emailRegister emailRegister-posts" type="email" placeholder="Nhập địa chỉ email"/>
-                                    <button className="emailSubmit emailSubmit-posts" type="submit">ĐĂNG KÝ</button>
+                                    <input id="registeredEmail" className="emailRegister emailRegister-posts" type="email" placeholder="Nhập địa chỉ email"/>
+                                    <button className="emailSubmit emailSubmit-posts" onClick={this.sendMail} type="submit">ĐĂNG KÝ</button>
                                 </div>
                             </div>
                         </div>
