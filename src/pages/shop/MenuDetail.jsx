bfacfe876d91e2fef4a301e83f287db745c03d87
Thanh Ha Ng Le <harley.ngx@gmail.com>
2020-04-30 02:53:30 +0700

    Basic UI for detail page #20

---
+++ src/pages/shop/MenuDetail.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Helmet } from 'react-helmet'
+import config from '../../config.json'
+
+
+export default class MenuDetail extends Component {
+    state = {
+        category: '',
+        name: '',
+        price: '',
+        description: '',
+        img_link: ''
+    }
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        axios
+            .get(config.rootApi + `menu/${id}`)
+            .then(data => {
+                const dataApi = data.data.data;
+
+                this.setState({ 
+                    category: dataApi.category,
+                    name: dataApi.name,
+                    price: dataApi.price,
+                    description: dataApi.description,
+                    img_link: dataApi.img_link
+                })
+            })
+            .catch(err => console.error(err))
+    }
+    render() {
+        return (
+            <div className="pageContent">
+                <Helmet>
+                    <title>{this.state.name}</title>
+                </Helmet>
+                <div className="detailContainer">
+                    <div className="row">
+                        <div className="col-6 detail-img">
+                            <img src={this.state.img_link} alt={this.state.description}/>
+                        </div>
+                        <div className="col-6 detail-content">
+                            <h3>{this.state.name}</h3>
+                            <h5><strong dangerouslySetInnerHTML={{__html: this.state.price}}/></h5>
+                            <p>{this.state.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
